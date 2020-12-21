6d9173e7e968a8e2b9999552e4278a563895801b
DawnDesign7 <dawnmoraa@gmail.com>
2020-12-21 03:54:47 +0300

    create function that allows for hide/show of description of services when icon is clicked.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Toggle What We do
+//design
+function showDesign() {
+  var hide = document.getElementById("design");
+  if (hide.style.display === "none") {
+    hide.style.display = "block";
+  } else {
+    hide.style.display = "none";
+  }
+}
+//development
+function showDevelopment() {
+  var hide = document.getElementById("development");
+  if (hide.style.display === "none") {
+    hide.style.display = "block";
+  } else {
+    hide.style.display = "none";
+  }
+}
+//product
+function showProduct() {
+  var hide = document.getElementById("product");
+  if (hide.style.display === "none") {
+    hide.style.display = "block";
+  } else {
+    hide.style.display = "none";
+  }
+}
