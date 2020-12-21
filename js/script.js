ce7bc3bd20a49da369b416a8868266b23fd6af7d
DawnDesign7 <dawnmoraa@gmail.com>
2020-12-21 06:11:43 +0300

    ALert messages when name and email address are not input. Business logic end updated to trigger submit button when enter key is hit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//User Interface
+
 // Toggle What We do
 //design
 function showDesign() {
@@ -27,4 +29,32 @@ function showProduct() {
   }
 }
 
-//Hover effect
+
+
+//Business Logic
+
+$(document).getElementById(function() 
+  $('#clientForm').keyup(function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementByNmae("contact").click();
+     }
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    var key = e.which;
+    if (key == 13) {
+      if (name == "") {
+      alert("Name is Missing"); //alert if name is not input
+      } else if (email == "") {
+      alert("Email address is missing"); //alert if email is not input
+      } else if (message == "") {
+      alert("Message is missing"); // alert if message is not input
+      } else {
+      $('#clientForm').submit();
+      alert("Thank you for visiting our page! We have received your messgae and we will respond promptly.");  //let client know message has been received.
+      }
+      return false;
+      }
+  });
+});
